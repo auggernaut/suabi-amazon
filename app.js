472fb72ed8a962391d34430f50bf8e57ee713e13
Augustin Bralley <augman@gmail.com>
2017-08-16 13:47:24 -0700

    allow cross origin *

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
